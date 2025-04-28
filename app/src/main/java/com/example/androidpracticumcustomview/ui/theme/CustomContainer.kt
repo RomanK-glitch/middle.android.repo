9b406cda63a04bdb9898ff38d18cbe8bff87fb31
rkostenko <r.kostenko@dataphone.su>
2025-04-28 22:34:44 +0300

    Custom ViewGroup and custom compose container implementation

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
@@ -1,5 +1,7 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
@@ -17,24 +19,111 @@ import android.widget.FrameLayout
 class CustomContainer @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
+    private val firstChild: View?
+        get() = if (childCount > 0) getChildAt(0) else null
+    private val secondChild: View?
+        get() = if (childCount > 1) getChildAt(1) else null
+
+    private val firstAlphaAnimator = ObjectAnimator().apply {
+        setPropertyName("alpha")
+        setFloatValues(0f, 1f)
+        duration = 2000
+    }
+    private val firstPositionAnimator = ValueAnimator().apply {
+        duration = 5000
+        addUpdateListener { animation ->
+            firstChild?.translationY = animation.animatedValue as Float
+        }
+    }
+
+    private val secondAlphaAnimator = ObjectAnimator().apply {
+        setPropertyName("alpha")
+        setFloatValues(0f, 1f)
+        duration = 2000
+    }
+    private val secondPositionAnimator = ValueAnimator().apply {
+        duration = 5000
+        addUpdateListener { animation ->
+            secondChild?.translationY = animation.animatedValue as Float
+        }
+    }
 
     init {
         setWillNotDraw(false)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        // TODO
-        // ...
+        checkChildCount()
+
+        firstChild?.let { measureChildView(it, widthMeasureSpec, heightMeasureSpec) }
+        secondChild?.let { measureChildView(it, widthMeasureSpec, heightMeasureSpec) }
+
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+
+        setMeasuredDimension(width, height)
+    }
+
+    private fun measureChildView(child: View, widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val childWidthSpec =
+            MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.AT_MOST)
+        val childHeightSpec =
+            MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(heightMeasureSpec), MeasureSpec.AT_MOST)
+
+        child.measure(childWidthSpec, childHeightSpec)
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        // TODO
-        // ...
+        val firstChildLeft = ((right - left) / 2) - (firstChild?.measuredWidth?.div(2) ?: 0)
+        firstChild?.layout(
+            firstChildLeft,
+            top,
+            firstChildLeft + (firstChild?.measuredWidth ?: 0),
+            top + (firstChild?.measuredHeight ?: 0)
+        )
+        if (!firstPositionAnimator.isStarted && childCount == 1) {
+            val firstPositionAnimationStart =
+                ((bottom - top) / 2) - (firstChild?.measuredHeight?.div(2) ?: 0)
+            firstPositionAnimator.apply {
+                setFloatValues(firstPositionAnimationStart.toFloat(), top.toFloat())
+                start()
+            }
+        }
+
+        val secondChildLeft = ((right - left) / 2) - (secondChild?.measuredWidth?.div(2) ?: 0)
+        secondChild?.layout(
+            secondChildLeft,
+            bottom - (secondChild?.measuredHeight ?: 0),
+            secondChildLeft + (secondChild?.measuredWidth ?: 0),
+            bottom
+        )
+        if (!secondPositionAnimator.isStarted && childCount == 2) {
+            val secondPositionAnimationStart =
+                ((top - bottom) / 2) + (secondChild?.measuredHeight?.div(2) ?: 0)
+            secondPositionAnimator.apply {
+                setFloatValues(secondPositionAnimationStart.toFloat(), top.toFloat())
+                start()
+            }
+        }
     }
 
     override fun addView(child: View) {
-        // TODO
-        // ...
+        checkChildCount()
+
+        super.addView(child)
+
+        if (childCount == 1) {
+            firstAlphaAnimator.target = firstChild
+            firstAlphaAnimator.start()
+        }
+
+        if (childCount == 2) {
+            secondAlphaAnimator.target = secondChild
+            secondAlphaAnimator.start()
+        }
+    }
+
+    private fun checkChildCount() {
+        if (childCount > 2) throw IllegalStateException()
     }
 }
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -1,13 +1,17 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 /*
 Задание:
@@ -24,15 +28,20 @@ fun MainScreen(closeActivity: () -> Unit) {
                 .clickable { closeActivity.invoke() },
             contentAlignment = Alignment.Center
         ) {
-
             CustomContainerCompose(
                 firstChild = {
-                    // TODO
-                    // ...
+                    Box(
+                        modifier = Modifier
+                            .size(150.dp)
+                            .background(Color.Cyan)
+                    )
                 },
                 secondChild = {
-                    // TODO
-                    // ...
+                    Box(
+                        modifier = Modifier
+                            .size(150.dp)
+                            .background(Color.Magenta)
+                    )
                 }
             )
         }
